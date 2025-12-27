d5ebedf01418d3b1fab8ca5abe8611c7d5dbf17e
Paul Damrongpiriyapong <pauldamrong@gmail.com>
2025-12-27 12:31:59 -0800

    Update 404 and added animation to homepage buttons

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,8 @@
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return <div className="h-screen content-center">
+    <div className="grid grid-cols-1 gap-8 justify-items-center">
+      <h1 className="text-5xl tracking-widest">404 - Page Not Found</h1>
+    </div>
+  </div>
+  return 
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,21 @@ export default function Page() {
     <div className="grid grid-cols-1 gap-8 justify-items-center">
       <h1 className="text-5xl tracking-widest">PAUL DAMRONGPIRIYAPONG</h1>
       <div className="text-2xl pb-5 space-x-5">
-        <Link href="/travels">[travels]</Link>
-        <Link href="/notes">[notes]</Link>
-        <Link href="/food">[food]</Link>
-        <Link href="/work">[work]</Link>
+        <Link href="/travels"><HoverFunction text="travel" /></Link>
+        <Link href="/food"><HoverFunction text="food" /></Link>
+        <Link href="/work"><HoverFunction text="work" /></Link>
+        <Link href="/notes"><HoverFunction text="notes" /></Link>
       </div>
     </div>
   </div>
+}
+
+function HoverFunction({ text }: { text: string }) {
+  return (
+    <div className="group inline-flex cursor-pointer items-center">
+      <span className="transition-transform duration-300 ease-out group-hover:-translate-x-1">[</span>
+      {text}
+      <span className="transition-transform duration-300 ease-out group-hover:translate-x-1">]</span>
+    </div>
+  )
 }
