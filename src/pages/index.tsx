41aea3a5d9dd42d7147a9058e212a80fa63044ca
Paul Damrongpiriyapong <pauldamrong@gmail.com>
2026-01-02 18:17:34 -0800

    Add Head to index, travels, and work pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 import Link from 'next/link'
+import Head from 'next/head'
 import HoverText from 'components/ui/HoverText'
 
 export default function Page() {
-  return <div className="h-screen content-center">
-    <div className="grid grid-cols-1 gap-8 justify-items-center">
-      <h1 className="text-3xl tracking-widest">- UNDER CONSTRUCTION -</h1>
-      <h1 className="text-5xl tracking-widest">PAUL DAMRONGPIRIYAPONG</h1>
-      <div className="text-2xl pb-5 space-x-5">
-        <Link href="/travels"><HoverText text="travels" /></Link>
-        <Link href="/food"><HoverText text="food" /></Link>
-        <Link href="/work"><HoverText text="work" /></Link>
-        <Link href="/notes"><HoverText text="notes" /></Link>
+  return (
+    <div>
+      <Head>
+        <title>Paul Damrongpiriyapong</title>
+      </Head>
+      <div className="h-screen content-center">
+        <div className="grid grid-cols-1 gap-8 justify-items-center">
+          <h1 className="text-3xl tracking-widest">- UNDER CONSTRUCTION -</h1>
+          <h1 className="text-5xl tracking-widest">PAUL DAMRONGPIRIYAPONG</h1>
+          <div className="text-2xl pb-5 space-x-5">
+            <Link href="/travels"><HoverText text="travels" /></Link>
+            <Link href="/food"><HoverText text="food" /></Link>
+            <Link href="/work"><HoverText text="work" /></Link>
+            <Link href="/notes"><HoverText text="notes" /></Link>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  )
 }
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,4 +1,5 @@
 import Header from 'components/common/Header'
+import Head from 'next/head'
 import type { GetStaticProps } from 'next';
 import Retrivers from "posts/retrievers";
 
@@ -8,11 +9,16 @@ interface WorkPageProps {
 
 export default function Work({ experiences }: WorkPageProps) {
   return (
-    <div className="h-screen w-1/2 mx-auto p-24">
-      <div className="grid grid-cols-1 gap-1">
-        <Header headerTitle="PAUL'S WORK" />
-        <div className="font-sans mb-28">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-        {experiences.map((experience, index) => (<WorkExperience key={index} {...experience} />))}
+    <div>
+      <Head>
+        <title>Paul&apos;s Work</title>
+      </Head>
+      <div className="h-screen w-1/2 mx-auto p-24">
+        <div className="grid grid-cols-1 gap-1">
+          <Header headerTitle="PAUL'S WORK" />
+          <div className="font-sans mb-28">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          {experiences.map((experience, index) => (<WorkExperience key={index} {...experience} />))}
+        </div>
       </div>
     </div>
   )
