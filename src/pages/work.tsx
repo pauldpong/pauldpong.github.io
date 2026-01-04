147354afa490cef1dc47572f736b56514e0f359a
Paul Damrongpiriyapong <pauldamrong@gmail.com>
2026-01-03 20:25:34 -0800

    Finalize work timeline component
    * Add support for links from work experience YAML file via `dangerouslySetInnerHTML`
    * Support responsive design for "work" page
    * Update description of "Dot Health" work experience

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -13,11 +13,15 @@ export default function Work({ experiences }: WorkPageProps) {
       <Head>
         <title>Paul&apos;s Work</title>
       </Head>
-      <div className="h-screen w-1/2 mx-auto p-24">
+      <div className="h-screen w-full md:w-3/4 xl:w-1/2 mx-auto p-5 md:p-15 xl:p-20">
         <div className="grid grid-cols-1 gap-1">
           <Header headerTitle="PAUL'S WORK" />
-          <div className="font-sans mb-28">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-          {experiences.map((experience, index) => (<WorkExperience key={index} {...experience} />))}
+          <div className="font-sans mb-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          <div className="flex flex-row">
+            <div className="grid grid-cols-[max-content_max-content_1fr] gap-x-5 mb-20">
+              {experiences.map((experience, index) => (<WorkExperience key={index} {...experience} />))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -42,33 +46,20 @@ interface WorkExperienceProps {
 }
 
 function WorkExperience({ employer, date, role, description }: WorkExperienceProps) {
-  // need to evaluate as a whole set of experience rather than one at a time
+  const markup = { __html: description };
+
   return (
-    <div className="grid grid-cols-[max-content_1fr] gap-5">
-      <div className="justify-items-end bg-green-200">
-        <div className="flex flex-row items-center">
-          <div className="text-2xl whitespace-pre-line me-5 ">{date}</div>
-          <div className="w-5 h-5 bg-black rounded-full"></div>
-        </div>
+    <>
+      <div className="text-2xl text-end whitespace-pre-line">{date}</div>
+      <div>
+        <div className="mt-1.5 w-5 h-5 bg-black rounded-full"></div>
+        <div className="relative -top-5 -z-1 mr-2.25 border-r-2 border-dashed border-black h-full"></div>
       </div>
-      <div className="bg-red-200">
+      <div className="mb-5">
         <div className="text-2xl">{employer}</div>
         <div className="text-xl mb-5">{role}</div>
-        <div className="text-xl whitespace-pre-line">{description}</div>
+        <div className="prose text-xl whitespace-pre-line text-black" dangerouslySetInnerHTML={markup} />
       </div>
-    </div>
+    </>
   )
-  // return (
-  //   <div className="flex flex-row gap-5">
-  //     <div className="flex-none flex flex-row h-min items-center">
-  //       <div className="text-2xl whitespace-pre-line me-5 ">{date}</div>
-  //       <div className="w-5 h-5 bg-black rounded-full"></div>
-  //     </div>
-  //     <div className="flex flex-col">
-  //       <div className="text-2xl">{employer}</div>
-  //       <div className="text-xl mb-5">{role}</div>
-  //       <div className="text-xl whitespace-pre-line">{description}</div>
-  //     </div>
-  //   </div>
-  // )
 }
