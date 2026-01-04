854167dedf65016b6e49cf248390288975c3d188
Paul Damrongpiriyapong <pauldamrong@gmail.com>
2026-01-04 11:57:02 -0800

    Tweak 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,8 @@
 export default function Custom404() {
   return (
-    <div className="h-screen content-center">
-      <div className="grid grid-cols-1 gap-8 justify-items-center">
-        <h1 className="text-5xl tracking-widest">404 - Page Not Found</h1>
-      </div>
+    <div className="h-screen content-center justify-items-center">
+      <h1 className="mb-5 text-4xl font-mono">404</h1>
+      <h1 className="text-2xl">The page was not found.</h1>
     </div>
   );
-  return;
 }
