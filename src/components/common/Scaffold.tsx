ba7448d0cf80817fd7b0f81af1dc45b0e95a6a81
Paul Damrongpiriyapong <pauldamrong@gmail.com>
2026-01-05 20:12:32 -0800

    Fix landing page on small screens

--- src/components/common/Scaffold.tsx
+++ src/components/common/Scaffold.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-interface ScaffoldProps extends React.PropsWithChildren {}
-
-export default function Scaffold({ children }: ScaffoldProps) {
+export default function Scaffold({ children }) {
   return (
     <div className="h-screen w-full md:w-3/4 xl:w-1/2 mx-auto p-5 md:p-15 xl:p-20">
       {children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,9 +10,10 @@ export default function Page() {
       </Head>
       <div className="h-screen content-center">
         <div className="grid grid-cols-1 gap-8 justify-items-center">
-          <h1 className="text-3xl tracking-widest">- UNDER CONSTRUCTION -</h1>
-          <h1 className="text-5xl tracking-widest">PAUL DAMRONGPIRIYAPONG</h1>
-          <div className="text-2xl pb-5 space-x-5">
+          <h1 className="text-3xl xl:text-5xl tracking-wide xl:tracking-widest">
+            PAUL DAMRONGPIRIYAPONG
+          </h1>
+          <div className="text-xl xl:text-2xl pb-5 space-x-5">
             <Link href="/travels">
               <HoverText text="travels" />
             </Link>
